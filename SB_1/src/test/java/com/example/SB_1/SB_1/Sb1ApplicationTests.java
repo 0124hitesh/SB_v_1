ca61d2b9ebc044302c196a3e72b5f7ee534041ec
Hitesh Sharma <hsharma@ingenuitygaming.com>
2024-09-01 13:03:10 +0530

    SB setup

---
+++ SB_1/src/test/java/com/example/SB_1/SB_1/Sb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SB_1.SB_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

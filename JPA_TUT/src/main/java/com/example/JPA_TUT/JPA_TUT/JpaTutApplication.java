4e2a34e329cdbf894b387c4573f9b1038c7b7c6f
0124hitesh <0124shivamsharma@gmail.com>
2024-10-24 22:33:31 +0530

    3.2 - Hibernate ORM

---
+++ JPA_TUT/src/main/java/com/example/JPA_TUT/JPA_TUT/JpaTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPA_TUT.JPA_TUT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTutApplication.class, args);
+	}
+
+}
---
+++ JPA_TUT/src/test/java/com/example/JPA_TUT/JPA_TUT/JpaTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPA_TUT.JPA_TUT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cea14b24476ae1e5824af173b38321bff55fd350
0124hitesh <0124shivamsharma@gmail.com>
2024-10-25 22:18:47 +0530

    3.3 JPA

---
+++ JPA_TUT/src/main/java/com/example/JPA_TUT/JPA_TUT/Repositories/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.example.JPA_TUT.JPA_TUT.Repositories;
+
+import com.example.JPA_TUT.JPA_TUT.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    Optional<ProductEntity> findByTitle(String pepsi);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    @Query("select e.title, e.price from ProductEntity e where e.title=:title and e.price=:price")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+}
--- JPA_TUT/src/test/java/com/example/JPA_TUT/JPA_TUT/JpaTutApplicationTests.java
+++ JPA_TUT/src/test/java/com/example/JPA_TUT/JPA_TUT/JpaTutApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.JPA_TUT.JPA_TUT;
 
+import com.example.JPA_TUT.JPA_TUT.Repositories.ProductRepository;
+import com.example.JPA_TUT.JPA_TUT.entities.ProductEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class JpaTutApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder()
+				.sku("nestle234")
+				.title("Nestle Chocolate")
+				.price(BigDecimal.valueOf(123.45))
+				.quantity(12)
+				.build();
+		ProductEntity savedProductEntity = productRepository.save(productEntity);
+		System.out.println(savedProductEntity);
+	}
+
+	@Test
+	void getRepository() {
+//		List<ProductEntity> entities = productRepository.findAll();
+//		List<ProductEntity> entities = productRepository.findByCreatedAtAfter(LocalDateTime.of(2024, 1, 1, 0, 0, 0));
+		List<ProductEntity> entities = productRepository.findByQuantityAndPrice(12, BigDecimal.valueOf(123.45));
+
+		System.out.println(entities);
+	}
+
+	@Test
+	void getSingleFromRepository() {
+		Optional<ProductEntity> productEntity = productRepository
+				.findByTitleAndPrice("Peps", BigDecimal.valueOf(14.4));
+		productEntity.ifPresent(System.out::println);
 	}
 
 }
